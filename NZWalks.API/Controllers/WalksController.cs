90a9ff6796e64ea64015044881f35d1cdee5778d
mohitacharya4 <mohitacharya4@gmail.com>
2023-05-07 13:25:14 +0530

    Added Filtering, sorting and pagination to getAllWalks method

--- NZWalks.API/Controllers/WalksController.cs
+++ NZWalks.API/Controllers/WalksController.cs
@@ -32,13 +32,14 @@ namespace NZWalks.API.Controllers
         }
 
         // GET Walks
-        // GET: /api/walks?filterOn=Name&filterQuery=Track
+        // GET: /api/walks?filterOn=Name&filterQuery=Track&sortBy=Name&isAscending=true&pageNumber=1&pageSize=10
         [HttpGet]
-        public async Task<IActionResult> GetAllAsync([FromQuery] string? filterOn, [FromQuery] string? filterQuery)
+        public async Task<IActionResult> GetAllAsync([FromQuery] string? filterOn, [FromQuery] string? filterQuery, [FromQuery] string? sortBy, [FromQuery] bool? isAscending, [FromQuery] int pageNumber = 1,
+            [FromQuery] int pageSize = 1000)
         {
-            var walksDomainModel = await walkRepository.GetAllAsync();
+            var walksDomainModel = await walkRepository.GetAllAsync(filterOn, filterQuery, sortBy, isAscending ?? true, pageNumber, pageSize);
 
-            return Ok(mapper.Map<List<WalkDto>>(walksDomainModel)); 
+            return Ok(mapper.Map<List<WalkDto>>(walksDomainModel));
         }
 
         [HttpGet]
--- NZWalks.API/Repositories/IWalkRepository.cs
+++ NZWalks.API/Repositories/IWalkRepository.cs
@@ -6,7 +6,7 @@ namespace NZWalks.API.Repositories
     {
         // Repositories always deals with Domain Models
         Task<Walk> CreateAsync(Walk walk);
-        Task<List<Walk>> GetAllAsync(string? filterOn = null, string? filterQuery = null);
+        Task<List<Walk>> GetAllAsync(string? filterOn = null, string? filterQuery = null, string? sortBy = null, bool? isAscending = true, int pageNumber = 1, int pageSize = 1000);
         Task<Walk?> GetByIdAsync(Guid id);
         Task<Walk?> UpdateAsync(Guid id, Walk walk);
         Task<Walk?> DeleteAsync(Guid id);
